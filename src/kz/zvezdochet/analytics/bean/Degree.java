c5e4f4efaea584f25556bae389f77f660758910b
Nataly Didenko <inbox@zvezdochet.guru>
2017-12-02 21:20:25 +0600

    Убрала хардкод

--- src/kz/zvezdochet/analytics/bean/Degree.java
+++ src/kz/zvezdochet/analytics/bean/Degree.java
@@ -1,11 +1,8 @@
 package kz.zvezdochet.analytics.bean;
 
-import java.util.Arrays;
-
 import kz.zvezdochet.analytics.service.DegreeService;
 import kz.zvezdochet.core.bean.TextGenderDictionary;
 import kz.zvezdochet.core.service.ModelService;
-import kz.zvezdochet.core.util.CalcUtil;
 
 /**
  * Зодиакальный градус
@@ -14,30 +11,40 @@ import kz.zvezdochet.core.util.CalcUtil;
 public class Degree extends TextGenderDictionary {
 	private static final long serialVersionUID = -3521775467191351633L;
 
-	/**
-	 * Проверка, является ли градус королевским
-	 * @param coord координата
-	 * @return true|false королевский|иной
-	 */
-	public static boolean isRoyal(double coord) {
-		int[] royals = {18, 69, 127, 175, 223, 281, 330};
-		int d = CalcUtil.trunc(coord);
-		return Arrays.asList(royals).contains(d); 
-	}
-
-	/**
-	 * Проверка, является ли градус разрушительным
-	 * @param coord координата
-	 * @return true|false разрушительный|иной
-	 */
-	public static boolean isDestructive(double coord) {
-		int[] royals = {23, 73, 130, 181, 229, 289, 334};
-		int d = CalcUtil.trunc(coord);
-		return Arrays.asList(royals).contains(d); 
-	}
-
 	@Override
 	public ModelService getService() {
 		return new DegreeService();
+	}
+
+	/**
+	 * Градус с позитивным толкованием
+	 */
+	private boolean positive;
+	/**
+	 * Королевский
+	 */
+	private boolean royal;
+	/**
+	 * Разрушительный
+	 */
+	private boolean destructive;
+
+	public boolean isPositive() {
+		return positive;
+	}
+	public void setPositive(boolean positive) {
+		this.positive = positive;
+	}
+	public boolean isRoyal() {
+		return royal;
+	}
+	public void setRoyal(boolean royal) {
+		this.royal = royal;
+	}
+	public boolean isDestructive() {
+		return destructive;
+	}
+	public void setDestructive(boolean destructive) {
+		this.destructive = destructive;
 	}
 }
--- src/kz/zvezdochet/analytics/service/DegreeService.java
+++ src/kz/zvezdochet/analytics/service/DegreeService.java
@@ -1,7 +1,11 @@
 package kz.zvezdochet.analytics.service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import kz.zvezdochet.analytics.bean.Degree;
 import kz.zvezdochet.core.bean.Model;
+import kz.zvezdochet.core.service.DataAccessException;
 import kz.zvezdochet.core.service.TextGenderDictionaryService;
 
 /**
@@ -17,5 +21,15 @@ public class DegreeService extends TextGenderDictionaryService {
 	@Override
 	public Model create() {
 		return new Degree();
+	}
+
+	@Override
+	public Degree init(ResultSet rs, Model model) throws DataAccessException, SQLException {
+		Degree dict = (model != null) ? (Degree)model : (Degree)create();
+		dict = (Degree)super.init(rs, model);
+		dict.setPositive(rs.getBoolean("positive"));
+		dict.setRoyal(rs.getBoolean("royal"));
+		dict.setDestructive(rs.getBoolean("destructive"));
+		return dict;
 	}
 }
