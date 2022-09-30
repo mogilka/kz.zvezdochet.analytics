a10f11b14efd55f2e43fb912c430058a8caa712e
natalie <natalie@natalie-Inspiron-3847>
2022-09-30 10:10:15 +0600

    Набросок глобального анализа

---
+++ src/kz/zvezdochet/analytics/handler/TransitChartHandler.java
@@ -0,0 +1,260 @@
+package kz.zvezdochet.analytics.handler;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.e4.core.contexts.Active;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.ChapterAutoNumber;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import kz.zvezdochet.analytics.Activator;
+import kz.zvezdochet.analytics.part.GraphicPart;
+import kz.zvezdochet.bean.Aspect;
+import kz.zvezdochet.bean.Event;
+import kz.zvezdochet.bean.Ingress;
+import kz.zvezdochet.bean.Place;
+import kz.zvezdochet.bean.Planet;
+import kz.zvezdochet.bean.SkyPoint;
+import kz.zvezdochet.bean.SkyPointAspect;
+import kz.zvezdochet.core.bean.Model;
+import kz.zvezdochet.core.handler.Handler;
+import kz.zvezdochet.core.util.DateUtil;
+import kz.zvezdochet.core.util.PlatformUtil;
+import kz.zvezdochet.export.handler.PageEventHandler;
+import kz.zvezdochet.export.util.PDFUtil;
+import kz.zvezdochet.service.PlanetService;
+
+/**
+ * Генерация графиков глобального прогноза за указанный период
+ * @author Natalie Didenko
+ */
+public class TransitChartHandler extends Handler {
+	private BaseFont baseFont;
+
+	public TransitChartHandler() {
+		super();
+		try {
+			baseFont = PDFUtil.getBaseFont();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Execute
+	public void execute(@Active MPart activePart) {
+		Document doc = new Document(PageSize.A4.rotate());
+		try {
+			long duration = System.currentTimeMillis();
+			long run = duration;
+			GraphicPart periodPart = (GraphicPart)activePart.getObject();
+			if (!periodPart.check(0)) return;
+
+			Map<Long, Model> planets = new PlanetService().getMap();
+			updateStatus("Расчёт транзитов на период", false);
+
+			Date initDate = periodPart.getInitialDate();
+			Date finalDate = periodPart.getFinalDate();
+			Calendar start = Calendar.getInstance();
+			start.setTime(initDate);
+
+			Calendar end = Calendar.getInstance();
+			end.setTime(finalDate);
+
+			String filename = PlatformUtil.getPath(Activator.PLUGIN_ID, "/out/charts.pdf").getPath();
+			PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(filename));
+	        writer.setPageEvent(new PageEventHandler());
+	        doc.open();
+
+	    	Font font = PDFUtil.getRegularFont();
+
+	        //metadata
+	        PDFUtil.getMetaData(doc, "Транзиты");
+
+	        //раздел
+			Chapter chapter = new ChapterAutoNumber("Транзиты");
+			chapter.setNumberDepth(0);
+
+			//шапка
+			Paragraph p = new Paragraph();
+			PDFUtil.printHeader(p, "Транзиты", null);
+			chapter.add(p);
+
+			SimpleDateFormat sdf = new SimpleDateFormat("EEEE, d MMMM yyyy");
+			String text = sdf.format(initDate) + " — " + sdf.format(finalDate);
+			p = new Paragraph(text, font);
+	        p.setAlignment(Element.ALIGN_CENTER);
+			chapter.add(p);
+
+			Font fontgray = PDFUtil.getAnnotationFont(false);
+			text = "Дата составления: " + DateUtil.fulldtf.format(new Date());
+			p = new Paragraph(text, fontgray);
+	        p.setAlignment(Element.ALIGN_CENTER);
+			chapter.add(p);
+
+			p = new Paragraph();
+	        p.setAlignment(Element.ALIGN_CENTER);
+			p.setSpacingAfter(20);
+	        p.add(new Chunk("Автор: ", fontgray));
+	        Chunk chunk = new Chunk(PDFUtil.AUTHOR, new Font(baseFont, 10, Font.UNDERLINE, PDFUtil.FONTCOLOR));
+	        chunk.setAnchor(PDFUtil.WEBSITE);
+	        p.add(chunk);
+	        chapter.add(p);
+
+			List<Long> ydates = new ArrayList<Long>();
+			Map<Long, Map<Long, List<SkyPointAspect>>> yplanets = new TreeMap<Long, Map<Long, List<SkyPointAspect>>>();
+
+			System.out.println("Prepared for: " + (System.currentTimeMillis() - run));
+			run = System.currentTimeMillis();
+
+			//разбивка дат по годам и месяцам
+			for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTime(date);
+				long time = date.getTime(); 
+				ydates.add(time);
+			}
+			Collections.sort(ydates);
+
+			/**
+			 * коды ингрессий, используемых в отчёте
+			 */
+			String[] icodes = new String[] {
+				Ingress._EXACT,
+				Ingress._SEPARATION,
+				Ingress._RETRO, Ingress._DIRECT,
+				Ingress._REPEAT
+			};
+
+			//создаём аналогичный массив, но с домами вместо дат
+			int k = -1;
+			for (Long time : ydates) {
+				++k;
+				Date date = new Date(time);
+				String sdate = DateUtil.formatCustomDateTime(date, "yyyy-MM-dd") + " 12:00:00";
+				Event event = new Event();
+				Date edate = DateUtil.getDatabaseDateTime(sdate);
+				event.setBirth(edate);
+				event.setPlace(new Place().getDefault());
+				event.setZone(0);
+				event.calc(true);
+
+				Map<String, List<Object>> ingressList = event.getPrev().initIngresses(event, false);
+				if (ingressList.isEmpty())
+					continue;
+
+				for (Map.Entry<String, List<Object>> ientry : ingressList.entrySet()) {
+					String key = ientry.getKey();
+					if (!Arrays.asList(icodes).contains(key))
+						continue;
+
+					if (key.contains("REPEAT")
+							&& (k > 0
+								&& !DateUtil.formatDate(date).equals(DateUtil.formatDate(finalDate))))
+						continue;
+
+					List<Object> ingresses = ientry.getValue();
+					for (Object object : ingresses) {
+						if (object instanceof Planet)
+							continue;
+						SkyPointAspect spa = (SkyPointAspect)object;
+						Planet skyPoint = (Planet)spa.getSkyPoint1();
+						long pid = skyPoint.getId();
+						if (20 == pid)
+							continue;
+						if (26 == pid)
+							continue;
+
+						SkyPoint skyPoint2 = spa.getSkyPoint2();
+						long pid2 = skyPoint2.getId();
+						if (20 == pid2)
+							continue;
+						if (22 == pid2)
+							continue;
+						if (26 == pid2)
+							continue;
+						if (pid == pid2)
+							continue;
+						if (21 == pid2 && 22 == pid)
+							continue;
+
+						Map<Long, List<SkyPointAspect>> dmap = yplanets.containsKey(pid2) ? yplanets.get(pid2) : new TreeMap<Long, List<SkyPointAspect>>();
+						List<SkyPointAspect> pmap = dmap.containsKey(time) ? dmap.get(time) : new ArrayList<SkyPointAspect>();
+						pmap.add(spa);
+						dmap.put(time, pmap);
+						yplanets.put(pid2, dmap);
+					}
+				}
+			}
+			ydates = null;
+			System.out.println("Composed for: " + (System.currentTimeMillis() - run));
+			SimpleDateFormat sdf2 = DateUtil.dbdf;
+
+			//генерируем транзиты планет
+			for (Map.Entry<Long, Map<Long, List<SkyPointAspect>>> entry : yplanets.entrySet()) {
+				Planet planet = (Planet)planets.get(entry.getKey());
+
+	        	Map<Long, List<SkyPointAspect>> map = entry.getValue();
+				if (map.isEmpty())
+					continue;
+
+				DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+				for (Map.Entry<Long, List<SkyPointAspect>> entry3 : map.entrySet()) {
+					List<SkyPointAspect> series = entry3.getValue();
+					if (null == series || series.isEmpty())
+						continue;
+
+					Long d = entry3.getKey();
+					for (int j = 0; j < series.size(); j++) {
+						SkyPointAspect spa = series.get(j);
+						Planet planet2 = (Planet)planets.get(spa.getSkyPoint1().getId());
+						Aspect aspect = spa.getAspect();
+						String sign = "";
+						if (!aspect.getCode().equals("CONJUNCTION"))
+							sign += aspect.getType().getSymbol();
+						dataset.addValue(planet2.getNumber(), planet2.getCode() + sign, sdf2.format(new Date(d)));
+					}
+				}
+				if (dataset.getColumnCount() > 0) {
+		        	Section section = PDFUtil.printSection(chapter, planet.getName(), null);
+					Image image = PDFUtil.printLineChart(writer, "", "", "Баллы", dataset, 750, 300, true);
+					section.add(image);
+					section.add(Chunk.NEXTPAGE);
+				}
+			}
+			yplanets = null;
+			doc.add(chapter);
+			doc.add(Chunk.NEWLINE);
+	        doc.add(PDFUtil.printCopyright());
+
+	        long time = System.currentTimeMillis();
+			System.out.println("Finished for: " + (time - run));
+			System.out.println("Duration: " + (time - duration));
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+	        doc.close();
+		}
+	}
+}
--- src/kz/zvezdochet/analytics/part/GraphicPart.java
+++ src/kz/zvezdochet/analytics/part/GraphicPart.java
@@ -1,16 +1,9 @@
 package kz.zvezdochet.analytics.part;
 
-import java.awt.BasicStroke;
-import java.awt.Stroke;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.GregorianCalendar;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -18,44 +11,23 @@ import javax.inject.Inject;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.time.TimeSeriesDataItem;
-import org.jfree.experimental.chart.swt.ChartComposite;
 
-import kz.zvezdochet.bean.Event;
-import kz.zvezdochet.bean.Planet;
-import kz.zvezdochet.core.bean.Model;
-import kz.zvezdochet.core.ui.util.GUIutil;
+import kz.zvezdochet.core.service.DataAccessException;
+import kz.zvezdochet.core.ui.util.DialogUtil;
+import kz.zvezdochet.core.ui.view.ListView;
 import kz.zvezdochet.core.ui.view.View;
 import kz.zvezdochet.core.util.DateUtil;
-import kz.zvezdochet.service.EventService;
-import kz.zvezdochet.service.PlanetService;
 
 /**
  * Представление графиков
  * @author Natalie Didenko
  *
  */
-public class GraphicPart extends View {
+public class GraphicPart extends ListView {
 
 	@Inject
 	public GraphicPart() {}
@@ -65,177 +37,87 @@ public class GraphicPart extends View {
 		return super.create(parent);
 	}
 
+	private DateTime dt;
+	private DateTime dt2;
+
 	@Override
-	protected void init(Composite parent) {
-		super.init(parent);
-		Group grFilter = new Group(sashForm, SWT.NONE);
-		grFilter.setText("Поиск");
-		grFilter.setLayout(new GridLayout());
+	public boolean check(int mode) throws Exception {
+		Date date = getInitialDate();
+		Date date2 = getFinalDate();
+		if (!DateUtil.isDateRangeValid(date, date2)) {
+			DialogUtil.alertWarning("Укажите правильный период");
+			return false;
+		}
+		return true;
+	}
+
+	public Date getInitialDate() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, dt.getDay());
+		calendar.set(Calendar.MONTH, dt.getMonth());
+		calendar.set(Calendar.YEAR, dt.getYear());
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		return calendar.getTime();
+	}
+
+	public Date getFinalDate() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, dt2.getDay());
+		calendar.set(Calendar.MONTH, dt2.getMonth());
+		calendar.set(Calendar.YEAR, dt2.getYear());
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		return calendar.getTime();
+	}
+
+	@Override
+	public void initFilter(Composite parent) {
+		grFilter = new Group(parent, SWT.NONE);
+		grFilter.setText("Период");
 
 		Label lb = new Label(grFilter, SWT.NONE);
 		lb.setText("Начало");
-		final DateTime dt = new DateTime(grFilter, SWT.DROP_DOWN);
-//		dt.setNullText(""); //$NON-NLS-1$
-//		dt.setSelection(selection);
+		dt = new DateTime(grFilter, SWT.DROP_DOWN);
 
 		lb = new Label(grFilter, SWT.NONE);
 		lb.setText("Конец");
-		final DateTime dt2 = new DateTime(grFilter, SWT.DROP_DOWN);
-//		dt2.setNullText(""); //$NON-NLS-1$
+		dt2 = new DateTime(grFilter, SWT.DROP_DOWN);
 
-		Button bt = new Button(grFilter, SWT.NONE);
-		bt.setText("Искать");
-		bt.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-//				if (null == dt.getSelection() || null == dt2.getSelection())
-//					return;
-				try {
-					Calendar calendar = Calendar.getInstance();
-					calendar.set(Calendar.DAY_OF_MONTH, dt.getDay());
-					calendar.set(Calendar.MONTH, dt.getMonth());
-					calendar.set(Calendar.YEAR, dt.getYear());
-					Date date = calendar.getTime();
-
-					calendar = Calendar.getInstance();
-					calendar.set(Calendar.DAY_OF_MONTH, dt2.getDay());
-					calendar.set(Calendar.MONTH, dt2.getMonth());
-					calendar.set(Calendar.YEAR, dt2.getYear());
-					Date date2 = calendar.getTime();
-
-					setData(date, date2);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			}
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
-		});
-
-		GridLayoutFactory.swtDefaults().applyTo(grFilter);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(grFilter);
-
-		group = new Group(parent, SWT.EMBEDDED);
-		group.setText("Инфографика");
-//		group.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_BLUE));
-		GridLayoutFactory.swtDefaults().applyTo(group);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(group);
-//		setData(new Date(1517206791), new Date(1517293190));
+		GridLayoutFactory.swtDefaults().numColumns(10).applyTo(grFilter);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(grFilter);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).
+			grab(true, false).applyTo(dt);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).
+			grab(true, false).applyTo(dt2);
 	}
 
-	protected void setData(Date initDate, Date finalDate) {
+	@SuppressWarnings("unlikely-arg-type")
+	@Override
+	protected void initControls() throws DataAccessException {
+		super.initControls();
 		try {
-//			for(Control control : group.getChildren())
-//				if (!control.isDisposed())
-//					control.dispose();
-			//разбивка дат по периоду
-			Calendar start = Calendar.getInstance();
-			start.setTime(initDate);
-	
-			Calendar end = Calendar.getInstance();
-			end.setTime(finalDate);
-	
-			Map<Long, Event> dates = new HashMap<>();
-			EventService service = new EventService();
-			for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(date);
-	
-				//ищем событие даты
-				Event event = null;
-				String sdate = DateUtil.formatCustomDateTime(date, "yyyy-MM-dd");
-				List<Event> events = service.findByDate(sdate, 1);
-				if (null == events || 0 == events.size()) {
-					//если нет, создаём
-					event = new Event(DateUtil.getDatabaseDateTime(sdate + " 12:00:00"), "Мой гороскоп");
-					event.calc(true);
-					event.setCalculated(true);
-					event.save();
-					System.out.println("Новый добавлен: " + event.toLog() + "\n");
-				} else {
-					event = events.get(0);
-					event.initData(false);
-				}
-				long time = event.getBirth().getTime(); 
-				if (!dates.containsKey(time))
-					dates.put(time, event);
-			}
-	//		Collections.sort(dates); TODO
-	
-			Map<Long, List<TimeSeriesDataItem>> items = new TreeMap<Long, List<TimeSeriesDataItem>>();
-			for (Map.Entry<Long, Event> entry : dates.entrySet()) {
-				Date date = new Date(entry.getKey());
-				Event event = entry.getValue();
-				Collection<Planet> planets = event.getPlanets().values();
-	
-				for (Planet planet : planets) {
-					long pid = planet.getId();
-					List<TimeSeriesDataItem> series = items.containsKey(pid) ? items.get(pid) : new ArrayList<TimeSeriesDataItem>();
-					TimeSeriesDataItem tsdi = new TimeSeriesDataItem(new Day(date), planet.getLongitude());
-					if (!series.contains(tsdi))
-						series.add(tsdi);
-					items.put(pid, series);
-				}
-			}
-	
-			//генерируем диаграмму орбит
-			TimeSeriesCollection dataset = new TimeSeriesCollection();
-			PlanetService pservice = new PlanetService();
-			List<Model> planets = pservice.getList();
-	       	for (Model model : planets) {
-	       		Planet planet = (Planet)model;
-	       		long pid = planet.getId();
-	       		List<TimeSeriesDataItem> series = items.containsKey(pid) ? items.get(pid) : new ArrayList<TimeSeriesDataItem>();
-	       		if (null == series || 0 == series.size())
-	       			continue;
-				TimeSeries timeSeries = new TimeSeries(planet.getSymbol());
-				for (TimeSeriesDataItem tsdi : series)
-					timeSeries.add(tsdi);
-				dataset.addSeries(timeSeries);
-	       	}
-	       	if (dataset.getSeriesCount() > 0) {
-	       		JFreeChart chart = ChartFactory.createTimeSeriesChart("Орбиты", "Даты", "Координаты", dataset, true, true, true);
-	            XYPlot plot = (XYPlot)chart.getPlot();
-	            plot.setBackgroundPaint(new java.awt.Color(230, 230, 250));
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.set(Calendar.DAY_OF_MONTH, 1);
+			int month = calendar.get(Calendar.MONTH);
+			dt.setDate(calendar.get(Calendar.YEAR), month, calendar.get(Calendar.DAY_OF_MONTH));
 
-                DateAxis axis = (DateAxis)plot.getDomainAxis();
-                axis.setDateFormatOverride(new SimpleDateFormat("dd.MM"));
-                axis.setAutoTickUnitSelection(false);
-                axis.setVerticalTickLabels(true);
-
-                final XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer)plot.getRenderer();
-                renderer.setBaseStroke(new BasicStroke(3));
-                Stroke dashed = new BasicStroke(3.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[] {3.0f}, 0.0f);
-                int scnt = dataset.getSeries().size();
-                for (int i = 0; i < scnt; i++) {
-                	Stroke stroke = renderer.getBaseStroke();
-                	if (i % 2 > 0)
-						stroke = dashed;
-                	renderer.setSeriesStroke(i, stroke);
-                }
-	            final Display display = Display.getDefault();
-	            Shell shell = new Shell(display);
-	            Point point = GUIutil.getScreenSize();
-	            shell.setSize(point.x - 800, point.y - 400);
-	            shell.setLayout(new FillLayout());
-	            shell.setText("Орбиты");
-	            ChartComposite frame = new ChartComposite(shell, SWT.NONE, chart, true);
-	            frame.setDisplayToolTips(true);
-	            frame.setHorizontalAxisTrace(false);
-	            frame.setVerticalAxisTrace(false);
-	            shell.open();
-	            while (!shell.isDisposed()) {
-	                if (!display.readAndDispatch())
-	                    display.sleep();
-	            }
-			}
+			int date = 31;
+			if (1 == month)
+				date = calendar.isLeapYear(calendar.get(Calendar.YEAR)) ? 28 : 29;
+			else if (Arrays.asList(new int[] {4, 6, 9, 11}).contains(month))
+				date = 30;
+			dt2.setDate(calendar.get(Calendar.YEAR), month, date);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Контейнер для графика
-	 */
-	private Group group;
+	@Override
+	protected String[] initTableColumns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
